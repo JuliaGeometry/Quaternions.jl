c9e43fc495ef926e0ac7360d2459bb68684c5c92
Adam Nemecek <adamnemecek@gmail.com>
2018-06-13 11:32:06 -0700

    added ispure for quaternion

--- src/Quaternion.jl
+++ src/Quaternion.jl
@@ -48,6 +48,8 @@ abs_imag(q::Quaternion) = sqrt(q.v1 * q.v1 + q.v2 * q.v2 + q.v3 * q.v3)
 abs2(q::Quaternion) = q.s * q.s + q.v1 * q.v1 + q.v2 * q.v2 + q.v3 * q.v3
 inv(q::Quaternion) = q.norm ? conj(q) : conj(q) / abs2(q)
 
+ispure(q::Quaternion) = iszero(real(q))
+
 isfinite(q::Quaternion) = q.norm ? true : (isfinite(q.s) && isfinite(q.v1) && isfinite(q.v2) && isfinite(q.v3))
 
 function normalize(q::Quaternion)
