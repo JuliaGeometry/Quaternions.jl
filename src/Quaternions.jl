6ec600827a543c7edaf645ba0ce6b8f20f55fea5
Sungwoo Jeong <sungwoo@Sungwoos-MacBook-Pro.local>
2021-04-18 09:43:35 +0900

    fix reviews

--- src/Quaternions.jl
+++ src/Quaternions.jl
@@ -4,8 +4,8 @@ module Quaternions
 
   import Base: +, -, *, /, ^
   import Base: abs, abs2, angle, conj, cos, exp, inv, isfinite, log, real, sin, sqrt
-  import Base: convert, promote_rule, float, imag
-  import LinearAlgebra: norm, normalize, exp
+  import Base: convert, promote_rule, float
+  import LinearAlgebra: norm, normalize
 
   include("Quaternion.jl")
   include("Octonion.jl")
