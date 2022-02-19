23a8212b1761c200030aa6614c4e0dbf971a0d9f
bzinberg <bzinberg@mit.edu>
2022-02-18 21:45:45 -0500

    Remove unnecessary type conversion inside `qrotation` (#38)
    * Add failing regression test for https://github.com/JuliaGeometry/Quaternions.jl/issues/8#issuecomment-610640094
    
    * Remove unnecessary type conversion.
    
    Addresses https://github.com/JuliaGeometry/Quaternions.jl/issues/8.
    
    * minor: simplify the "test this doesn't crash" logic
    
    Co-authored-by: Seth Axen <seth.axen@gmail.com>
    
    Co-authored-by: Seth Axen <seth.axen@gmail.com>

--- test/test_Quaternion.jl
+++ test/test_Quaternion.jl
@@ -58,6 +58,15 @@ let # test rotations
     @test angle(qrotation([0, 1, 0], pi / 4)) ≈ pi / 4
     @test angle(qrotation([0, 0, 1], pi / 2)) ≈ pi / 2
 
+    # Regression test for
+    # https://github.com/JuliaGeometry/Quaternions.jl/issues/8#issuecomment-610640094
+    struct MyReal <: Real
+      val::Real
+    end
+    Base.:(/)(a::MyReal, b::Real) = a.val / b
+    # this used to throw an error
+    qrotation([1, 0, 0], MyReal(1.5))
+
     let # test numerical stability of angle
         ax = randn(3)
         for θ in [1e-9, π - 1e-9]
