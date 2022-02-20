5090d42a32f65a9edef457fdc69946a784bf3a0e
hyrodium <hyrodium@gmail.com>
2022-02-21 01:10:13 +0900

    add tests for exp

--- test/test_Quaternion.jl
+++ test/test_Quaternion.jl
@@ -139,3 +139,34 @@ for _ in 1:100
         @test q ⊗ linpol(q1, q2, t) ≈ linpol(q ⊗ q1, q ⊗ q2, t)
     end
 end
+
+@testset "exp" begin
+    @test exp(Quaternion(0, 0, 0, 0)) == Quaternion(1, 0, 0, 0, true)
+    @test exp(Quaternion(2, 0, 0, 0)) == Quaternion(exp(2), 0, 0, 0, false)
+    @test exp(Quaternion(0, 2, 0, 0)) == Quaternion(cos(2), sin(2), 0, 0, true)
+    @test exp(Quaternion(0, 0, 2, 0)) == Quaternion(cos(2), 0, sin(2), 0, true)
+    @test exp(Quaternion(0, 0, 0, 2)) == Quaternion(cos(2), 0, 0, sin(2), true)
+
+    @test norm(exp(Quaternion(0, 0, 0, 0))) ≈ 1
+    @test norm(exp(Quaternion(2, 0, 0, 0))) ≠ 1
+    @test norm(exp(Quaternion(0, 2, 0, 0))) ≈ 1
+    @test norm(exp(Quaternion(0, 0, 2, 0))) ≈ 1
+    @test norm(exp(Quaternion(0, 0, 0, 2))) ≈ 1
+
+    @test exp(Quaternion(0., 0., 0., 0.)) == Quaternion(1, 0, 0, 0, true)
+    @test exp(Quaternion(2., 0., 0., 0.)) == Quaternion(exp(2), 0, 0, 0, false)
+    @test exp(Quaternion(0., 2., 0., 0.)) == Quaternion(cos(2), sin(2), 0, 0, true)
+    @test exp(Quaternion(0., 0., 2., 0.)) == Quaternion(cos(2), 0, sin(2), 0, true)
+    @test exp(Quaternion(0., 0., 0., 2.)) == Quaternion(cos(2), 0, 0, sin(2), true)
+
+    @test norm(exp(Quaternion(0., 0., 0., 0.))) ≈ 1
+    @test norm(exp(Quaternion(2., 0., 0., 0.))) ≠ 1
+    @test norm(exp(Quaternion(0., 2., 0., 0.))) ≈ 1
+    @test norm(exp(Quaternion(0., 0., 2., 0.))) ≈ 1
+    @test norm(exp(Quaternion(0., 0., 0., 2.))) ≈ 1
+
+    @test exp(Quaternion(0,0,0,0)) isa Quaternion{Float64}
+    @test exp(Quaternion(0.,0,0,0)) isa Quaternion{Float64}
+    @test exp(Quaternion(0//1,0,0,0)) isa Quaternion{Float64}
+    @test exp(Quaternion(BigFloat(0),0,0,0)) isa Quaternion{BigFloat}
+end
