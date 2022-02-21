766add41cb246faed9fa6d403b2aace86a3303bb
Yuto Horikawa <hyrodium@gmail.com>
2022-02-21 21:46:31 +0900

    update exp tests for norm flag (#66)

--- test/test_Quaternion.jl
+++ test/test_Quaternion.jl
@@ -231,11 +231,11 @@ for _ in 1:100
 end
 
 @testset "exp" begin
-    @test exp(Quaternion(0, 0, 0, 0)) == Quaternion(1, 0, 0, 0, true)
-    @test exp(Quaternion(2, 0, 0, 0)) == Quaternion(exp(2), 0, 0, 0, false)
-    @test exp(Quaternion(0, 2, 0, 0)) == Quaternion(cos(2), sin(2), 0, 0, true)
-    @test exp(Quaternion(0, 0, 2, 0)) == Quaternion(cos(2), 0, sin(2), 0, true)
-    @test exp(Quaternion(0, 0, 0, 2)) == Quaternion(cos(2), 0, 0, sin(2), true)
+    @test exp(Quaternion(0, 0, 0, 0)) === Quaternion(1., 0., 0., 0., true)
+    @test exp(Quaternion(2, 0, 0, 0)) === Quaternion(exp(2), 0, 0, 0, false)
+    @test exp(Quaternion(0, 2, 0, 0)) === Quaternion(cos(2), sin(2), 0, 0, true)
+    @test exp(Quaternion(0, 0, 2, 0)) === Quaternion(cos(2), 0, sin(2), 0, true)
+    @test exp(Quaternion(0, 0, 0, 2)) === Quaternion(cos(2), 0, 0, sin(2), true)
 
     @test norm(exp(Quaternion(0, 0, 0, 0))) ≈ 1
     @test norm(exp(Quaternion(2, 0, 0, 0))) ≠ 1
@@ -243,11 +243,11 @@ end
     @test norm(exp(Quaternion(0, 0, 2, 0))) ≈ 1
     @test norm(exp(Quaternion(0, 0, 0, 2))) ≈ 1
 
-    @test exp(Quaternion(0., 0., 0., 0.)) == Quaternion(1, 0, 0, 0, true)
-    @test exp(Quaternion(2., 0., 0., 0.)) == Quaternion(exp(2), 0, 0, 0, false)
-    @test exp(Quaternion(0., 2., 0., 0.)) == Quaternion(cos(2), sin(2), 0, 0, true)
-    @test exp(Quaternion(0., 0., 2., 0.)) == Quaternion(cos(2), 0, sin(2), 0, true)
-    @test exp(Quaternion(0., 0., 0., 2.)) == Quaternion(cos(2), 0, 0, sin(2), true)
+    @test exp(Quaternion(0., 0., 0., 0.)) === Quaternion(1., 0., 0., 0., true)
+    @test exp(Quaternion(2., 0., 0., 0.)) === Quaternion(exp(2), 0, 0, 0, false)
+    @test exp(Quaternion(0., 2., 0., 0.)) === Quaternion(cos(2), sin(2), 0, 0, true)
+    @test exp(Quaternion(0., 0., 2., 0.)) === Quaternion(cos(2), 0, sin(2), 0, true)
+    @test exp(Quaternion(0., 0., 0., 2.)) === Quaternion(cos(2), 0, 0, sin(2), true)
 
     @test norm(exp(Quaternion(0., 0., 0., 0.))) ≈ 1
     @test norm(exp(Quaternion(2., 0., 0., 0.))) ≠ 1
