49a1f8d529b0280ac4bf757d2e4210b7f40d97de
Seth Axen <seth.axen@gmail.com>
2022-02-23 21:40:16 +0100

    Move helpers to own file

---
+++ test/helpers.jl
@@ -0,0 +1,7 @@
+test_associative(x, y, z, ⊗) = @test (x ⊗ y) ⊗ z ≈ x ⊗ (y ⊗ z)
+test_commutative(x, y, ⊗) = @test x ⊗ y ≈ y ⊗ x
+test_inverse(x, eins, ⊗, inv) = (@test x ⊗ inv(x) ≈ eins; @test inv(x) ⊗ x ≈ eins)
+test_neutral(x, eins, ⊗) = (@test x ⊗ eins ≈ x; @test eins ⊗ x ≈ x)
+test_monoid(x, y, z, ⊗, eins) = (test_associative(x, y, z, ⊗); test_neutral(x, eins, ⊗))
+test_group(x, y, z, ⊗, eins, inv) = (test_monoid(x, y, z, ⊗, eins);test_inverse(x, eins, ⊗, inv))
+test_multiplicative(x, y, ⊗, f) = @test f(x ⊗ y) ≈ f(x) ⊗ f(y)
