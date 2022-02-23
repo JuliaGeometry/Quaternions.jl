5b2794d620c137a8f530aa3f8d40a9edcab1186a
Seth Axen <seth.axen@gmail.com>
2022-02-23 21:39:27 +0100

    Move quaternion test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,4 +9,4 @@ test_monoid(x, y, z, ⊗, eins) = (test_associative(x, y, z, ⊗); test_neutral(
 test_group(x, y, z, ⊗, eins, inv) = (test_monoid(x, y, z, ⊗, eins);test_inverse(x, eins, ⊗, inv))
 test_multiplicative(x, y, ⊗, f) = @test f(x ⊗ y) ≈ f(x) ⊗ f(y)
 
-include("test_Quaternion.jl")
+include("Quaternion.jl")
