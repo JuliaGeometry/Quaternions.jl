362c168beb53b386d8e4286d7c73dfd708594040
hyrodium <hyrodium@gmail.com>
2022-10-16 10:51:48 +0900

    update wording

--- src/Quaternion.jl
+++ src/Quaternion.jl
@@ -44,7 +44,7 @@ Convert real numbers or arrays to quaternion. `i, j, k` defaults to zero.
 julia> quat(7)
 Quaternion{Int64}(7, 0, 0, 0, false)
 
-julia> quat([1, 2, 3])  # The output will be changed in the next breaking release for consistency. (#94)
+julia> quat([1, 2, 3])  # This output will be changed in the next breaking release for consistency. (#94)
 Quaternion{Int64}(0, 1, 2, 3, false)
 ```
 """
