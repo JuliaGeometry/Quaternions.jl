286452c0de0319fe34ff4fbb125802936036213b
Yuto Horikawa <hyrodium@gmail.com>
2022-10-17 20:36:47 +0900

    Fix docstring with plural
    Co-authored-by: Seth Axen <seth@sethaxen.com>

--- src/Quaternion.jl
+++ src/Quaternion.jl
@@ -1,7 +1,7 @@
 """
     Quaternion{T<:Real} <: Number
 
-Quaternion number type with real and imaginary part of type `T`.
+Quaternion number type with real and imaginary parts of type `T`.
 
 `QuaternionF16`, `QuaternionF32` and `QuaternionF64` are aliases for
 `Quaternion{Float16}`, `Quaternion{Float32}`, and `Quaternion{Float64}`, respectively.
