aa48327135403866eb3d35d62d06aba6f9dfc9c7
Yuto Horikawa <hyrodium@gmail.com>
2022-10-17 20:37:24 +0900

    Use Oxford comma in docstring
    Co-authored-by: Seth Axen <seth@sethaxen.com>

--- src/Quaternion.jl
+++ src/Quaternion.jl
@@ -3,7 +3,7 @@
 
 Quaternion number type with real and imaginary parts of type `T`.
 
-`QuaternionF16`, `QuaternionF32` and `QuaternionF64` are aliases for
+`QuaternionF16`, `QuaternionF32`, and `QuaternionF64` are aliases for
 `Quaternion{Float16}`, `Quaternion{Float32}`, and `Quaternion{Float64}`, respectively.
 
 See also: [`quat`](@ref), [`real`](@ref), [`imag_part`](@ref).
