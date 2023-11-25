8f4c091d18f7da161131239cf62775fc0880f0dc
hyrodium <hyrodium@gmail.com>
2023-11-25 12:32:42 +0900

    avoid tests on unbound_args

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,9 @@ using Quaternions
 using Aqua
 using RealDot
 
-Aqua.test_all(Quaternions)
+# Avoid tests on unbound_args.
+# https://github.com/JuliaGeometry/Quaternions.jl/pull/132#discussion_r1383817950
+Aqua.test_all(Quaternions, unbound_args=false)
 
 include("helpers.jl")
 include("Quaternion.jl")
